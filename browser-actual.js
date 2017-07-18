986ed8221613c8677489137893aebf254f92e37e
MichaelHBBrooks <Michael.H.B.Brooks@gmail.com>
2017-07-17 21:20:32 -0400

    Added IE support and browser string argument support to browser-actual.js

--- browser-actual.js
+++ browser-actual.js
@@ -1,48 +1,49 @@
 var webdriverio = require('webdriverio');
 
-var arg = process.argv[2];
+var first_arg = process.argv[2];
 
-var chrome_options = {
-    desiredCapabilities: {
-        browserName: 'chrome'
-    }
-};
-var firefox_options = {
-    desiredCapabilities: {
-        browserName: 'firefox'
-    }
-};
-var safari_options = {
-    desiredCapabilities: {
-        browserName: 'safari',
-    }
-};
+var browsers = getBrowserClients(first_arg);
 
-var browsers = [];
-browsers.push(webdriverio.remote(chrome_options));
-browsers.push(webdriverio.remote(firefox_options));
-browsers.push(webdriverio.remote(safari_options));
-
-var x = arg;
-browsers[x].init().then(function(asdf) {
-    console.log(asdf)
-
-    // browsers[x].getUrl().then(function(url) {
-    //     console.log('Success! URL: ' + url);
-    // }, function(err) {
-    //     console.log('Error encountered.' + err);
-    // });
-    browsers[x].end();
-},function(failure){
-    console.log('Unable to launch '+browsers[x].desiredCapabilities.browserName)
+browsers.forEach(function(browser) {
+    browser.init().then(function(browser_result) {
+        recordResult(browser_result);
+        browser.end();
+    }, function(failure) {
+        console.log('Unable to launch "' + browser.desiredCapabilities.browserName + '"');
+    });
 });
 
+function getBrowserClients(browser_type) {
+    var browsers = [];
+    if (browser_type === 'all') {
+        browsers.push(webdriverio.remote(getCapabilitiesJSON('chrome')));
+        browsers.push(webdriverio.remote(getCapabilitiesJSON('firefox')));
+        // browsers.push(webdriverio.remote(getCapabilitiesJSON('safari')));
+        browsers.push(webdriverio.remote(getCapabilitiesJSON('internet explorer')));
+    } else {
+        browsers.push(webdriverio.remote(getCapabilitiesJSON(browser_type)));
+    }
+    return browsers;
+}
 
-// webdriverio
-//     .remote(options)
-//     .init()
-//     .url('http://www.google.com')
-//     .getTitle().then(function(title) {
-//         console.log('Title was: ' + title);
-//     })
-//     .end();
+function getCapabilitiesJSON(browser_type) {
+    var options = {
+        desiredCapabilities: {
+            browserName: browser_type
+        }
+    };
+    return options;
+}
+
+function recordResult(json) {
+    console.log(json);
+    var browser_name = json.value.browserName;
+    var browser_version;
+    if (browser_name === 'chrome' || browser_name === 'internet explorer') {
+        browser_version = json.value.version;
+    } else if (browser_name === 'firefox') {
+        browser_version = json.value.browserVersion;
+    }
+    console.log('Browser Name: ' + browser_name);
+    console.log('Browser Version: ' + browser_version);
+}
